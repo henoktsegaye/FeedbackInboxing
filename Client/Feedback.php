541f9d3757e2f4ec5ddb9bf781a41c0cf70a6087
HenokT <maxhenock@gmail.com>
2018-05-30 00:24:57 +0300

    feedback .php

--- Client/Feedback.php
+++ Client/Feedback.php
@@ -14,11 +14,11 @@ class InsertFeedback
 		$name = $_POST['first_name'].' '.$_POST['last_name'];
 		$disc = $_POST['description'];
 		$type = $_POST['group1'];
-		$datenow=
+		$datenow = new date("Y-m-d");
 	$query = "INSERT INTO Feedback(id ,name, description, type, date) values('','$name' , '$desc' , '$type' , '$datenow')";
 	require_once '../Admin/Database/Database.php';
 	$DB = new Database;
-	$connection = $DB.connect;
+	$connection = $DB->$connect;
 	$connection -> query($query);
 	header("location: Acknowlade.php");
 }
